ec18c15d1eb2358f5aa4368f019c4811eb8ab6af
Brian Valcarcel <brian.valcarcel98@gmail.com>
2019-12-02 13:24:25 -0500

    changed template

---
+++ app/src/main/java/com/example/pinplay/UserFragment.java
@@ -0,0 +1,65 @@
+package com.example.pinplay;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.pinplay.R;
+
+public class UserFragment extends DialogFragment implements View.OnClickListener {
+
+    private EditText mEditText;
+
+    public UserFragment() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static UserFragment newInstance(String title) {
+        UserFragment frag = new UserFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_user, container);
+        Button b = (Button) v.findViewById(R.id.button2);
+        b.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        // Get field from view
+//        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+//        // Fetch arguments from bundle and set title
+//        String title = getArguments().getString("title", "Enter Name");
+//        getDialog().setTitle(title);
+//        // Show soft keyboard automatically and request focus to field
+//        mEditText.requestFocus();
+//        getDialog().getWindow().setSoftInputMode(
+//                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button2:
+                MapsActivity.hideMarker();
+                this.dismiss();
+        }
+    }
+}
