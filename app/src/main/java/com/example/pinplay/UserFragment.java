be3eb0d517bc2149dda2a4b544aa558def1fd289
NSelf <nself@umd.edu>
2019-12-02 18:17:05 -0500

    Removed commented template code. Seeing results of Brian's merge

--- app/src/main/java/com/example/pinplay/UserFragment.java
+++ app/src/main/java/com/example/pinplay/UserFragment.java
@@ -40,19 +40,6 @@ public class UserFragment extends DialogFragment implements View.OnClickListener
         return v;
     }
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-//        // Get field from view
-//        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
-//        // Fetch arguments from bundle and set title
-//        String title = getArguments().getString("title", "Enter Name");
-//        getDialog().setTitle(title);
-//        // Show soft keyboard automatically and request focus to field
-//        mEditText.requestFocus();
-//        getDialog().getWindow().setSoftInputMode(
-//                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-    }
 
     @Override
     public void onClick(View v) {
